f375372db0683df7a787c750d60f492ef831b887
Tengku rafi <t.mrr39@gmail.com>
2023-12-27 15:57:01 +0700

    feat: first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+}));
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
