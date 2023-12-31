9ee66ae26384ded7764036bfb18f72f08332b953
Tengku rafi <t.mrr39@gmail.com>
2023-12-31 10:37:08 +0700

    feat: setup apolloclient

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <nav className='navbar mb-4 p-0'>
+        <div className="container">
+            <a href="" className='navbar-brand'>
+                <div className='d-flex'>
+                    <img src="" alt="" />
+                    <div>GraphQL</div>
+                </div>
+            </a>
+        </div>
+    </nav>
+  )
+}
